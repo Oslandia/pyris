c870f394adb411eec023330236d887bbf29b07dd
Damien Garaud <damien.garaud@gmail.com>
2016-03-18 18:02:05 +0100

    begin to write a setup.py file

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+
+with open("README.md") as fobj:
+    LONG_DESCRIPTION = fobj.read()
+
+
+setuptools.setup(
+    name='pyris',
+    version='0.1',
+    license='BSD',
+    url='https://github.com/garaud/pyris',
+    packages=setuptools.find_packages(exclude=['data']),
+    install_requires=["flask", "flask-restplus", "pyaml"],
+
+    author="Damien Garaud",
+    author_email='damien.garaud@gmail.com',
+    description="INSEE/IRIS geolocalization",
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ]
+)
