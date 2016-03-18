bcdbf655a607cf080b86c796fd50217a639ee668
Damien Garaud <damien.garaud@gmail.com>
2016-03-18 16:31:33 +0100

    data: slightly modify the script to insert data

--- data/insert_data.sh
+++ data/insert_data.sh
@@ -7,6 +7,8 @@
 # > create extension postgis;
 # with the superuser role
 
-shp2pgsql -W latin1 -I -s 4326 iris-2013-01-01.shp geoiris | psql -d pyris -U dgaraud
+user="user"
+
+shp2pgsql -W latin1 -I -s 4326 iris-2013-01-01.shp geoiris | psql -d pyris -U $user
 
 # It could be a quite long
