31f5355cd51ad06f1512ea6b48d7124591ed1800
Damien Garaud <damien.garaud@gmail.com>
2016-03-23 23:25:43 +0100

    also return the found address with lon,lat in address.py

--- pyris/address.py
+++ pyris/address.py
@@ -19,7 +19,16 @@ def coordinate(address):
     resources = api.search.get(q=address)
     if len(resources['features']) == 0:
         return None,None
-    return lonlat(resources["features"][0])
+    lon, lat = lonlat(resources["features"][0])
+    return {"lon": lon,
+            "lat": lat,
+            "address": retrieve_address(resources["features"][0])}
+
+
+def retrieve_address(feature):
+    """Retrieve the address from a feature
+    """
+    return feature["properties"]["label"]
 
 
 def lonlat(feature):
@@ -30,4 +39,4 @@ def lonlat(feature):
 
 if __name__ == '__main__':
     q = "place des quinconces bordeaux"
-    lon,lat = coordinate(q)
+    res = coordinate(q)
