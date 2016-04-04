eb31aa48d82aba0fdc42b6409f1e2a29e74a3e28
Damien Garaud <damien.garaud@gmail.com>
2016-04-05 01:35:54 +0200

    data: add more psql lines to delete duplications

--- data/insert_data.sh
+++ data/insert_data.sh
@@ -10,3 +10,7 @@
 user="$USERNAME"
 
 shp2pgsql -D -W latin1 -I -s 4326 iris-2013-01-01.shp geoiris | psql -d pyris -U $user
+
+# don't know why but there are several duplications in the shapefile (same geometries for the same IRIS)
+psql pyris -c "DELETE FROM geoiris WHERE gid IN (SELECT gid FROM (SELECT gid,RANK() OVER (PARTITION BY dcomiris ORDER BY gid) FROM geoiris) AS X WHERE X.rank > 1);
+"
