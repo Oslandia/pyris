f73c2538b7a60f969f58cc759e992d1f82c4b3f7
Damien Garaud <damien.garaud@gmail.com>
2016-04-06 00:30:25 +0200

    data: improve insert-data shell script

--- data/03-insert-data.sh
+++ data/03-insert-data.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
+set -e
+
 # You need to install PostGIS
 # Suppose the database 'pyris' exists
 
-# In your database, first do
-# > create extension postgis;
-# with the superuser role
-
-user="$USERNAME"
-
-shp2pgsql -D -W latin1 -I -s 4326 iris-2013-01-01.shp geoiris | psql -d pyris -U $user
+echo "use shp2pgsql to insert some data from the shp file"
+shp2pgsql -D -W latin1 -I -s 4326 iris-2013-01-01.shp geoiris | psql -d pyris
 
 # don't know why but there are several duplications in the shapefile (same geometries for the same IRIS)
+echo "data cleaning: remove some duplicated rows"
 psql pyris -c "DELETE FROM geoiris WHERE gid IN (SELECT gid FROM (SELECT gid,RANK() OVER (PARTITION BY dcomiris ORDER BY gid) FROM geoiris) AS X WHERE X.rank > 1);
 "
