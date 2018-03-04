d4654fcef9a2b9eba45436f4833bd34d63c6b568
Damien Garaud <damien.garaud@oslandia.com>
2018-03-04 17:01:40 +0100

    change 'men' to 'male' in the SQL variable name

--- pyris/api/queries/iris_population_sex.sql
+++ pyris/api/queries/iris_population_sex.sql
@@ -11,11 +11,11 @@ SELECT
   ,p13_f4559 as female_45_59
   ,p13_f6074 as female_60_74
   ,p13_f75p as female_75
-  ,p13_h0014 as men_0_14
-  ,p13_h1529 as men_15_29
-  ,p13_h3044 as men_30_44
-  ,p13_h4559 as men_45_59
-  ,p13_h6074 as men_60_74
-  ,p13_h75p as men_75
+  ,p13_h0014 as male_0_14
+  ,p13_h1529 as male_15_29
+  ,p13_h3044 as male_30_44
+  ,p13_h4559 as male_45_59
+  ,p13_h6074 as male_60_74
+  ,p13_h75p as male_75
 FROM insee.population
 WHERE iris = %s;
